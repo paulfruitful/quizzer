621b76eeb0bb2ee1568766a85d44a19794f54db7
paulfruitful <fruitful2007@outlook.com>
2024-10-31 12:43:28 +0000

    Set up

---
+++ app/_components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col p-6 '>
+
+        <h1>Quizzer </h1>
+      
+    </div>
+  )
+}
+
+export default Hero
